c0ff5ab75f60d8cc8e60fcc864045c0e776f2933
Ahmet Emin Yazgan <ahmeteminyazgan5858@gmail.com>
2024-02-08 10:34:58 +0100

    first commit - initialization

---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,57 @@
+import {
+  Avatar,
+  Dropdown,
+  DropdownDivider,
+  DropdownHeader,
+  DropdownItem,
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from "flowbite-react";
+
+export default function Nav() {
+  return (
+    <Navbar rounded>
+      <NavbarBrand href="https://flowbite-react.com">
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Flowbite React
+        </span>
+      </NavbarBrand>
+      <div className="flex md:order-2">
+        <Dropdown
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar
+              alt="User settings"
+              img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+              rounded
+            />
+          }
+        >
+          <DropdownHeader>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">
+              name@flowbite.com
+            </span>
+          </DropdownHeader>
+          <DropdownItem>Dashboard</DropdownItem>
+          <DropdownItem>Settings</DropdownItem>
+          <DropdownItem>Earnings</DropdownItem>
+          <DropdownDivider />
+          <DropdownItem>Sign out</DropdownItem>
+        </Dropdown>
+        <NavbarToggle />
+      </div>
+      <NavbarCollapse>
+        <NavbarLink href="/" active>
+          TODOS
+        </NavbarLink>
+        <NavbarLink href="/register">Register</NavbarLink>
+        <NavbarLink href="/login">Login</NavbarLink>
+      </NavbarCollapse>
+    </Navbar>
+  );
+}
