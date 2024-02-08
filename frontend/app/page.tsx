dfa49581f8edb9b5320505e53300ea620f98cbcd
Ahmet Emin Yazgan <130467590+AhmetYazgan@users.noreply.github.com>
2024-02-08 23:34:30 +0100

    [feature/homepage] - create simple home page (#13)

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  <main>Home</main>;
+  <main className="text-center flex flex-col gap-6 max-w-4xl mx-auto mt-10 px-2">
+    <h1 className="text-gray-900 font-bold text-3xl">
+      Welcome to My <span className="text-[#1b7793]">TODO App</span>
+    </h1>
+    <p className="text-gray-800">
+      Manage your tasks, stay organized, and increase your productivity with our
+      user-friendly todo app. Whether it's daily chores, work assignments, or
+      personal goals, you can keep track of everything in one place. Create,
+      edit, and mark tasks as completed to stay on top of your to-do list
+      effortlessly.
+    </p>
+  </main>;
 }
