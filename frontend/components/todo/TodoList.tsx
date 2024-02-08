ed5fe1f11f524c6d095d2952869ef213b16f9246
Ahmet Emin Yazgan <ahmeteminyazgan5858@gmail.com>
2024-02-08 23:24:29 +0100

    [chore/ui-improvements] - add style to placeholder texts

--- frontend/components/todo/TodoList.tsx
+++ frontend/components/todo/TodoList.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import TodoItem from './TodoItem'
-import { Accordion } from 'flowbite-react'
-import Link from 'next/link';
-import { getAllTodos } from './todos';
+import React from "react";
+import TodoItem from "./TodoItem";
+import { Accordion } from "flowbite-react";
+import Link from "next/link";
+import { getAllTodos } from "./todos";
 
 export const revalidate = 0;
 
@@ -10,28 +10,34 @@ export default async function TodoList() {
   const todos = await getAllTodos();
 
   return (
-    <div className='max-w-6xl mx-auto px-4 py-8'>
-      {todos.data ?
-        (todos.data.length > 0 ?
+    <div className="max-w-6xl mx-auto px-4 py-8">
+      {todos.data ? (
+        todos.data.length > 0 ? (
           <Accordion>
-            {todos.data.map(todo => (
-              <TodoItem
-                key={todo.id}
-                todo={todo}
-              />
+            {todos.data.map((todo) => (
+              <TodoItem key={todo.id} todo={todo} />
             ))}
-          </Accordion> :
-          <div>
-            <p>Looks like you don't have any task right now</p>
-            <Link href={"/add-todo"}>Create Todos</Link>
+          </Accordion>
+        ) : (
+          <div className="text-center">
+            <p className="mb-6 font-medium text-gray-800">
+              Looks like you don't have any tasks right now
+            </p>
+            <Link
+              href={"/add-todo"}
+              className="bg-blue-600 text-white px-5 py-2 rounded-2xl cursor-pointer hover:bg-blue-600/90"
+            >
+              Create Todos
+            </Link>
           </div>
-        ) :
-        <div>
-          <p>
+        )
+      ) : (
+        <div className="text-center">
+          <p className="font-medium text-gray-800">
             {`${todos.error} Please try again later.`}
           </p>
-        </div>      
-      }
+        </div>
+      )}
     </div>
-  )
+  );
 }
