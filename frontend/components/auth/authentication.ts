87fb2eb9631fdc839ee3daad6cda4510b98c7ffa
Ahmet Emin Yazgan <ahmeteminyazgan5858@gmail.com>
2024-02-09 14:15:59 +0100

    [feature/docker] - add Dockerfiles, create base_url for easier config

--- frontend/components/auth/authentication.ts
+++ frontend/components/auth/authentication.ts
@@ -1,136 +1,146 @@
-'use server';
+"use server";
 
-import { cookies } from 'next/headers'
-import axios from 'axios';
-import { LoggedInUser, LoginData, NewUser, RegisterUser, UserInfo } from '@/lib/auth/types';
+import { cookies } from "next/headers";
+import axios from "axios";
+import {
+  LoggedInUser,
+  LoginData,
+  NewUser,
+  RegisterUser,
+  UserInfo,
+} from "@/lib/auth/types";
+import { BASE_URL } from "@/lib/variables";
 
 const cookieStore = cookies();
 
 export const getUser = async () => {
-    const id = cookies().get('userId');
-    const token = cookies().get('token');
-    if (token && id) {
-        const url = `http://127.0.0.1:8000/users/user/${id.value}/`;
-        try {
-            const response = await axios.get(url, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Token ${token.value}`,
-                },
-            });
-            if (response.status === 200) {
-                const data: UserInfo = response.data;
-                return data;
-            } else {
-                return null;
-            }
-        } catch (error) {
-            throw error;
-        }
-    } else {
+  const id = cookies().get("userId");
+  const token = cookies().get("token");
+  if (token && id) {
+    const url = `${BASE_URL}users/user/${id.value}/`;
+    try {
+      const response = await axios.get(url, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Token ${token.value}`,
+        },
+      });
+      if (response.status === 200) {
+        const data: UserInfo = response.data;
+        return data;
+      } else {
         return null;
+      }
+    } catch (error) {
+      throw error;
     }
+  } else {
+    return null;
+  }
 };
 
 export async function storeToken(token: string) {
-    cookies().set({
-        name: 'token',
-        value: token,
-        secure: true,
-        httpOnly: true,
-        path: '/',
-        maxAge: 60 * 60
-    })
+  cookies().set({
+    name: "token",
+    value: token,
+    secure: true,
+    httpOnly: true,
+    path: "/",
+    maxAge: 60 * 60,
+  });
 }
 
 export async function storeId(id: number) {
-    cookies().set({
-        name: 'userId',
-        value: id.toString(),
-        secure: true,
-        httpOnly: true,
-        path: '/',
-        maxAge: 60 * 60
-    })
+  cookies().set({
+    name: "userId",
+    value: id.toString(),
+    secure: true,
+    httpOnly: true,
+    path: "/",
+    maxAge: 60 * 60,
+  });
 }
 
 export async function getUserToken() {
-    const token = cookieStore.get('token');
-    return token;
+  const token = cookieStore.get("token");
+  return token;
 }
 
 export async function getUserId() {
-    const userId = cookieStore.get('userId');
-    return userId;
+  const userId = cookieStore.get("userId");
+  return userId;
 }
 
 export async function deleteCookie(name: string) {
-    cookieStore.delete(name);
+  cookieStore.delete(name);
 }
 
 export const register = async (userData: RegisterUser) => {
-    try {
-        const response = await axios.post('http://127.0.0.1:8000/users/register/', {
-            username: userData.username,
-            email: userData.email,
-            password: userData.password,
-            password2: userData.repeatPassword
-        });
+  try {
+    const response = await axios.post(`${BASE_URL}users/register/`, {
+      username: userData.username,
+      email: userData.email,
+      password: userData.password,
+      password2: userData.repeatPassword,
+    });
 
-        if (response.status === 201) {
-            const data: NewUser = response.data;
-            storeToken(data.token);
-            storeId(data.id);
-            return { success: true };
-        } else {
-            return { success: false, error: "Something went wrong!" };
-        }
-    } catch (error) {
-        return { success: false, error: 'An error occurred' };
+    if (response.status === 201) {
+      const data: NewUser = response.data;
+      storeToken(data.token);
+      storeId(data.id);
+      return { success: true };
+    } else {
+      return { success: false, error: "Something went wrong!" };
     }
-}
+  } catch (error) {
+    return { success: false, error: "An error occurred" };
+  }
+};
 
 export const login = async (loginData: LoginData) => {
-    try {
-        const response = await axios.post('http://127.0.0.1:8000/auth/login/', {
-            username: loginData.username,
-            password: loginData.password,
-        });
+  try {
+    const response = await axios.post(`${BASE_URL}auth/login/`, {
+      username: loginData.username,
+      password: loginData.password,
+    });
 
-        if (response.status === 200) {
-            const data: LoggedInUser = response.data;
-            storeToken(data.key);
-            storeId(data.user.id);
-            return { success: true, error: "" };
-        } else {
-            return { success: false, error: "Something went wrong!" };
-        }
-    } catch (error) {
-        return { success: false, error: 'An error occurred. Please check your credentials.' };
+    if (response.status === 200) {
+      const data: LoggedInUser = response.data;
+      storeToken(data.key);
+      storeId(data.user.id);
+      return { success: true, error: "" };
+    } else {
+      return { success: false, error: "Something went wrong!" };
     }
+  } catch (error) {
+    return {
+      success: false,
+      error: "An error occurred. Please check your credentials.",
+    };
+  }
 };
 
 export const logout = async () => {
-    const token = cookies().get('token');
-    try {
-        const response = await axios.post(
-            'http://127.0.0.1:8000/auth/logout/',
-            {},
-            {
-                headers: {
-                    Authorization: `Token ${token?.value}`,
-                },
-            }
-        );
+  const token = cookies().get("token");
+  try {
+    const response = await axios.post(
+      `${BASE_URL}auth/logout/`,
+      {},
+      {
+        headers: {
+          Authorization: `Token ${token?.value}`,
+        },
+      }
+    );
 
-        if (response.status === 200) {
-            cookies().delete('userId');
-            cookies().delete('token');
-            return { success: true, error: "" };
-        } else {
-            return { success: false, error: "Something went wrong!" };
-        }
-    } catch (error) {
-        return { success: false, error: 'An error occurred' };
+    if (response.status === 200) {
+      cookies().delete("userId");
+      cookies().delete("token");
+      return { success: true, error: "" };
+    } else {
+      return { success: false, error: "Something went wrong!" };
     }
+  } catch (error) {
+    return { success: false, error: "An error occurred" };
+  }
 };
--- frontend/components/todo/TodoList.tsx
+++ frontend/components/todo/TodoList.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import TodoItem from './TodoItem'
-import { Accordion } from 'flowbite-react'
-import Link from 'next/link';
-import { getAllTodos } from './todos';
-import { Todo } from '@/lib/todo/types';
+import React from "react";
+import TodoItem from "./TodoItem";
+import { Accordion } from "flowbite-react";
+import Link from "next/link";
+import { getAllTodos } from "./todos";
+import { Todo } from "@/lib/todo/types";
 
 export const revalidate = 0;
 
@@ -11,26 +11,41 @@ const RenderTodos = ({ todos }: { todos: Todo[] | undefined }) => {
   if (todos) {
     return (
       <Accordion>
-        {todos.map(todo => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-          />
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
         ))}
       </Accordion>
-    )
+    );
   }
-}
+};
 
-export default async function TodoList({ searchParams }: { searchParams: { sort?: string } }) {
+export default async function TodoList({
+  searchParams,
+}: {
+  searchParams: { sort?: string };
+}) {
   let todos = await getAllTodos();
 
   const sortedByCreatedDate = todos.data?.slice().sort((a, b) => {
-    return new Date(searchParams.sort?.includes("-") ? b.created_date : a.created_date).getTime() - new Date(searchParams.sort?.includes("-") ? a.created_date : b.created_date).getTime();
+    return (
+      new Date(
+        searchParams.sort?.includes("-") ? b.created_date : a.created_date
+      ).getTime() -
+      new Date(
+        searchParams.sort?.includes("-") ? a.created_date : b.created_date
+      ).getTime()
+    );
   });
 
   const sortedByUpdatedDate = todos.data?.slice().sort((a, b) => {
-    return new Date(searchParams.sort?.includes("-") ? b.updated_date : a.updated_date).getTime() - new Date(searchParams.sort?.includes("-") ? a.updated_date : b.updated_date).getTime();
+    return (
+      new Date(
+        searchParams.sort?.includes("-") ? b.updated_date : a.updated_date
+      ).getTime() -
+      new Date(
+        searchParams.sort?.includes("-") ? a.updated_date : b.updated_date
+      ).getTime()
+    );
   });
 
   function createSortQuery(sort: string | undefined) {
@@ -53,47 +68,65 @@ export default async function TodoList({ searchParams }: { searchParams: { sort?
     }
   }
 
-
   if (!todos.data) {
     return (
-      <div className='max-w-6xl mx-auto px-4 py-8'>
-        <div className='text-center'>
-          <p className='font-medium text-gray-800'>
+      <div className="max-w-6xl mx-auto px-4 py-8">
+        <div className="text-center">
+          <p className="font-medium text-gray-800">
             {`${todos.error} Please try again later.`}
           </p>
         </div>
       </div>
-    )
+    );
   } else if (todos.data.length === 0) {
     return (
-      <div className='max-w-6xl mx-auto px-4 py-8'>
-        <div className='text-center'>
-          <p className='mb-6 font-medium text-gray-800'>Looks like you don't have any tasks right now</p>
-          <Link href={"/add-todo"} className='bg-blue-600 text-white px-5 py-2 rounded-2xl cursor-pointer hover:bg-blue-600/90'>Create Todos</Link>
+      <div className="max-w-6xl mx-auto px-4 py-8">
+        <div className="text-center">
+          <p className="mb-6 font-medium text-gray-800">
+            Looks like you don&#39;t have any tasks right now
+          </p>
+          <Link
+            href={"/add-todo"}
+            className="bg-blue-600 text-white px-5 py-2 rounded-2xl cursor-pointer hover:bg-blue-600/90"
+          >
+            Create Todos
+          </Link>
         </div>
       </div>
-    )
+    );
   } else {
     return (
-      <div className='max-w-6xl mx-auto px-4 py-8'>
-        <main className='flex flex-col gap-4'>
-          <div className='flex justify-between gap-2 sm:gap-4 items-center px-2'>
-            <p className='text-xs sm:text-sm'>Sort By</p>
-            <div className='flex gap-4'>
-              <Link href={`/todos?sort=${createSortQuery(searchParams.sort)}`} className="text-xs font-light hover:underline">Created Date</Link>
-              <Link href={`/todos?sort=${updateSortQuery(searchParams.sort)}`} className="text-xs font-light hover:underline">Updated Date</Link>
+      <div className="max-w-6xl mx-auto px-4 py-8">
+        <main className="flex flex-col gap-4">
+          <div className="flex justify-between gap-2 sm:gap-4 items-center px-2">
+            <p className="text-xs sm:text-sm">Sort By</p>
+            <div className="flex gap-4">
+              <Link
+                href={`/todos?sort=${createSortQuery(searchParams.sort)}`}
+                className="text-xs font-light hover:underline"
+              >
+                Created Date
+              </Link>
+              <Link
+                href={`/todos?sort=${updateSortQuery(searchParams.sort)}`}
+                className="text-xs font-light hover:underline"
+              >
+                Updated Date
+              </Link>
             </div>
           </div>
           <hr />
           {searchParams.sort ? (
-            searchParams.sort.includes("created_date") ?
-              <RenderTodos todos={sortedByCreatedDate} /> :
+            searchParams.sort.includes("created_date") ? (
+              <RenderTodos todos={sortedByCreatedDate} />
+            ) : (
               <RenderTodos todos={sortedByUpdatedDate} />
-          ) :
+            )
+          ) : (
             <RenderTodos todos={todos.data} />
-          }
+          )}
         </main>
       </div>
-    )
+    );
   }
 }
