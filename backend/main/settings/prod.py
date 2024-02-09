1a3cf48f9ba3e069cc4c11e9398ab8befee0c065
Burak Can Ates <burakcanates@gmail.com>
2024-02-09 17:18:01 +0100

    [feature/docker-compose] - docker compose configuration

--- backend/main/settings/prod.py
+++ backend/main/settings/prod.py
@@ -3,11 +3,11 @@ from .base import *
 DATABASES = {
   "default": {
     "ENGINE": "django.db.backends.postgresql_psycopg2",
-    "NAME": config("SQL_DATABASE"),
-    "USER": config("SQL_USER"),
-    "PASSWORD": config("SQL_PASSWORD"),
-    "HOST": config("SQL_HOST"),
-    "PORT": config("SQL_PORT"),
+    "NAME": config("POSTGRES_DATABASE"),
+    "USER": config("POSTGRES_USER"),
+    "PASSWORD": config("POSTGRES_PASSWORD"),
+    "HOST": config("POSTGRES_HOST"),
+    "PORT": config("POSTGRES_PORT"),
     "ATOMIC_REQUESTS": True,
   }
 }
